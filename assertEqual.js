46b824efb9fc047c71973ab9d21a821b8a8c7a0e
Mahad Abdirahman <14ma88@queensu.ca>
2022-05-05 20:52:34 -0400

    finished

---
+++ assertEqual.js
@@ -0,0 +1,30 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+if(actual === expected){
+let a = 'Assertion Passed:';
+let b =  actual;
+let c = '==='
+let d = expected;
+
+let e = a + b + c + d;
+
+  console.log(e);
+
+}else {
+let a = 'Assertion Failed:'
+let b = actual 
+let c = '!=='
+let d = expected 
+let e = a + b + c + d;
+console.log(e);
+}
+
+
+};
+// TEST CODE
+console.assert(assertEqual("Lighthouse Labs", "Bootcamp"))
+console.assert(assertEqual(1, 1))
+
+
+
+

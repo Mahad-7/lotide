95e5ac09ff9bf53e771c2429040ff45f9d8167a9
Mahad Abdirahman <14ma88@queensu.ca>
2022-05-06 18:10:51 -0400

    Head

--- head.js
+++ head.js
@@ -1,22 +1,51 @@
-const assertEqual = function(actual, expected) {
-  if(actual === expected){
-  let a = 'Assertion Passed:';
-  let b =  actual;
-  let c = '==='
-  let d = expected;
-  let e = a + b + c + d;
+//const assertEqual = function(actual, expected) {
+  //if(actual === expected){
+  //let a = 'Assertion Passed:';
+  //let b =  actual;
+  //let c = '==='
+  //let d = expected;
+  //let e = a + b + c + d;
    // console.log(e);
   
+  //}else {
+  //let a = 'Assertion Failed:'
+  //let b = actual 
+  //let c = '!=='
+ // let d = expected 
+  //let e = a + b + c + d;
+  //console.log(e);
+  //}
+  
+  //};
+  // TEST CODE
+  //console.assert(assertEqual("Lighthouse Labs", "Bootcamp"))
+  //console.assert(assertEqual(1, 1))
+
+  //top is first way of doing it
+//Bottom is second way 
+
+
+  // FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+  let output = `✅✅✅  Assertion Passed: ${actual} === ${expected}`
+    console.log(output);
+  
   }else {
-  let a = 'Assertion Failed:'
-  let b = actual 
-  let c = '!=='
-  let d = expected 
-  let e = a + b + c + d;
-  console.log(e);
+    let output = '🛑🛑🛑 Assertion Failed:' + actual + '!==' + expected;
+    console.log(output);
   }
   
   };
-  // TEST CODE
-  console.assert(assertEqual("Lighthouse Labs", "Bootcamp"))
-  console.assert(assertEqual(1, 1))
+
+const head = function (array){
+  return array[0]
+}
+//test
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+//test
+assertEqual(head([5]), 5)
+assertEqual(head([]), undefined)
+
